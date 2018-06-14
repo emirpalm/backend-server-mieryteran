493139d5e2c14f8dc67661d569679525d9de4430
Emir Nahin <emirpalm@outlook.com>
2018-06-14 17:50:18 -0500

    minor fix

--- routes/prueba.js
+++ routes/prueba.js
@@ -6,33 +6,52 @@ var Prueba = require('../models/prueba');
 var app = express();
 
 //mostramos todos los pedimentos 
-app.get("/", function(req, res) {
-    Prueba.getPedimentos(function(error, data) {
-        res.json(200, data);
+app.get("/", (req, res) => {
+    Prueba.getPedimentos((err, data) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al cargar los pedimentos',
+                errores: err
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            data
+        });
     });
 });
 
 //obtiene un pedimentos por su idPedimento
-app.get("/:id", function(req, res) {
+app.get("/:id", (req, res) => {
     //id del usuario
     var id = req.params.id;
     //solo actualizamos si la id es un número
     if (!isNaN(id)) {
-        Prueba.getPedimento(id, function(error, data) {
+        Prueba.getPedimento(id, (err, data) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar pedimento',
+                    errores: err
+                });
+            }
             //si el pedimento existe lo mostramos en formato json
             if (typeof data !== 'undefined' && data.length > 0) {
-                res.json(200, data);
-            }
-            //en otro caso mostramos una respuesta conforme no existe
-            else {
-                res.json(404, { "msg": "notExist" });
+                res.status(200).json({
+                    ok: true,
+                    data
+                });
+            } else {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El pedimento con el id ' + id + ' no existe',
+                    errores: { message: 'No existe un pedimento con ese ID' }
+                });
             }
         });
     }
-    //si hay algún error
-    else {
-        res.json(500, { "msg": "Error" });
-    }
+
 });
 
 // export
